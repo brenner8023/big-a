d89c75919aaec865805617ff079015224f92186f
brenner8023 <vue666@qq.com>
2025-11-26 21:52:05 +0800

    feat: add price_limit count

--- src/price_limit.js
+++ src/price_limit.js
@@ -1,59 +1,22 @@
-const fs = require('node:fs')
-const path = require('node:path')
-const { APP_DIR } = require('./config')
-
 exports.getPriceLimit = async function () {
-  const date = 20251116
-  const getUrl = () => {
+  const getUrl = (date) => {
     return `https://webrelease.dzh.com.cn/htmlweb/ztts/api.php?service=getZttdData&date=${date}`
-  }
-  const getListUrl = (secids) => {
-    const fields = 'f12,f14,f2,f3,f4,f25,f20,f13,f18,f6,f145,f100,f265,f266,f297'
-    return `https://push2delay.eastmoney.com/api/qt/ulist.np/get?fltt=2&fields=${fields}&secids=${secids.join(
-      ','
-    )}`
   }
   const headers = {
     'content-type': 'application/json',
   }
-  const res1 = await fetch(getUrl(), { headers })
-  const { data } = await res1.json()
-  const sectorMap = {}
-  if (data.length) {
-    const secids = data.map((item) => item.code.replace('SZ', '0.').replace('SH', '1.'))
-    const res2 = await fetch(getListUrl(secids), { headers })
-    const {
-      data: { diff },
-    } = await res2.json()
-    data.forEach((item) => {
-      if (item.name.includes('ST')) {
-        return
-      }
-      const code = item.code.replace('SZ', '').replace('SH', '')
-      const targetInfo = diff.find((i) => i.f12 === code)
-      const stockData = {
-        code: item.code,
-        name: item.name,
-      }
-      if (sectorMap[targetInfo.f100]) {
-        sectorMap[targetInfo.f100].push(stockData)
-      } else {
-        sectorMap[targetInfo.f100] = [stockData]
-      }
-    })
-  }
-  const result = []
-  Object.keys(sectorMap).forEach((key) => {
-    result.push({
-      sector: key,
-      stocks: sectorMap[key],
-    })
+  const dateArr = [
+    20251113, 20251114, 20251117, 20251118, 20251119,
+
+    20251120, 20251121, 20251124, 20251125, 20251126,
+  ]
+  const resList = await Promise.all(dateArr.map((date) => fetch(getUrl(date), { headers })))
+  const dataList = await Promise.all(resList.map((res) => res.json()))
+  let countArr = []
+  dataList.forEach((item) => {
+    countArr.push(item.data.filter((stock) => !stock.name.includes('ST')).length)
   })
-  result.sort((a, b) => b.stocks.length - a.stocks.length)
-  console.log(result.length)
-  const dir = path.join(APP_DIR, 'price_limit')
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir)
-  }
-  fs.writeFileSync(path.join(dir, `${date}.json`), JSON.stringify(result, null, 2))
+  console.log('涨停板统计: ', countArr)
+  console.log('平均: ', countArr.reduce((a, b) => a + b, 0) / 10)
+  return
 }
