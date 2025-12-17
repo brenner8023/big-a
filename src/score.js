7dc3f4db0d5a0eb6ad6d51646ab011de412c131c
brenner8023 <vue666@qq.com>
2025-12-17 21:13:08 +0800

    feat: add pos

--- src/score.js
+++ src/score.js
@@ -12,7 +12,7 @@ function main() {
     const currClose = dailyData[dailyData.length - 1][4]
 
     let volCount = 0
-    dailyData.slice(-5).forEach((item) => {
+    dailyData.slice(-4).forEach((item) => {
       const pct_chg = item[5]
       const volume = item[6]
       volCount += pct_chg >= 0 ? volume : -volume
@@ -23,7 +23,7 @@ function main() {
 
     const rules = [
       didi ? 0 : 1, // 收盘价同时小于昨日最低价和前日收盘价
-      volCount > 0 ? 1 : 0, // 5天内是否红肥绿瘦
+      volCount > 0 ? 1 : 0, // 4天内是否红肥绿瘦
       currClose >= bbi[bbi.length - 1] ? 1 : 0, // 收盘价是否在BBI上方
       isRsiUp(dailyData) ? 1 : 0, // RSI是否多头排列
     ]
